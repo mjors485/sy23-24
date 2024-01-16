5d23fc107d22e99b0dcf30f725da54e43aeefd46
Michael <mjors485@learner.dvusd.org>
2024-01-16 12:32:00 -0700

    friday absence update

--- Dex/Dex/Form1.vb
+++ Dex/Dex/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.IO
 
 Public Class Form1
+    Dim records(50) As String
     Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
         Field1.Text = ""
         Field2.Text = ""
@@ -35,8 +36,14 @@ Public Class Form1
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim inFile As New StreamReader("data.txt")
-        Field1.Text = inFile.ReadToEnd
-        inFile.Close()
+        If IO.File.Exists("data.txt") Then
+            Dim inFile As New StreamReader("data.txt")
+            Dim idx As Integer = 0
+            While (Not inFile.EndOfStream)
+                records(idx) = inFile.ReadLine
+                idx = idx + 1
+            End While
+            inFile.Close()
+        End If
     End Sub
 End Class
