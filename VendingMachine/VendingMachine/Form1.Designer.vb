75a890de512a5cd94d7f71da091d3044acfbaa79
Michael <mjors485@learner.dvusd.org>
2024-02-07 13:16:13 -0700

    vending machine start (VB)

---
+++ VendingMachine/VendingMachine/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.nButton = New System.Windows.Forms.Button()
+        Me.diButton = New System.Windows.Forms.Button()
+        Me.qButton = New System.Windows.Forms.Button()
+        Me.doButton = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'nButton
+        '
+        Me.nButton.Location = New System.Drawing.Point(630, 90)
+        Me.nButton.Name = "nButton"
+        Me.nButton.Size = New System.Drawing.Size(74, 23)
+        Me.nButton.TabIndex = 0
+        Me.nButton.Text = "Nickel"
+        Me.nButton.UseVisualStyleBackColor = True
+        '
+        'diButton
+        '
+        Me.diButton.Location = New System.Drawing.Point(710, 90)
+        Me.diButton.Name = "diButton"
+        Me.diButton.Size = New System.Drawing.Size(74, 23)
+        Me.diButton.TabIndex = 1
+        Me.diButton.Text = "Dime"
+        Me.diButton.UseVisualStyleBackColor = True
+        '
+        'qButton
+        '
+        Me.qButton.Location = New System.Drawing.Point(630, 204)
+        Me.qButton.Name = "qButton"
+        Me.qButton.Size = New System.Drawing.Size(74, 23)
+        Me.qButton.TabIndex = 2
+        Me.qButton.Text = "Quarter"
+        Me.qButton.UseVisualStyleBackColor = True
+        '
+        'doButton
+        '
+        Me.doButton.Location = New System.Drawing.Point(710, 204)
+        Me.doButton.Name = "doButton"
+        Me.doButton.Size = New System.Drawing.Size(74, 23)
+        Me.doButton.TabIndex = 3
+        Me.doButton.Text = "Dollar"
+        Me.doButton.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(630, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(74, 72)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(710, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(74, 72)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(630, 126)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(74, 72)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox3.TabIndex = 6
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(710, 126)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(74, 72)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox4.TabIndex = 7
+        Me.PictureBox4.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Ivory
+        Me.Label1.Location = New System.Drawing.Point(652, 239)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 37)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.doButton)
+        Me.Controls.Add(Me.qButton)
+        Me.Controls.Add(Me.diButton)
+        Me.Controls.Add(Me.nButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents nButton As Button
+    Friend WithEvents diButton As Button
+    Friend WithEvents qButton As Button
+    Friend WithEvents doButton As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ VendingMachine/VendingMachine/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Dim cs As New CoinSlot
+    Private Sub nButton_Click(sender As Object, e As EventArgs) Handles nButton.Click
+        cs.InsertNickel()
+        Label1.Text = cs.total
+    End Sub
+
+    Private Sub diButton_Click(sender As Object, e As EventArgs) Handles diButton.Click
+        cs.InsertDime()
+        Label1.Text = cs.total
+    End Sub
+
+    Private Sub qButton_Click(sender As Object, e As EventArgs) Handles qButton.Click
+        cs.InsertQuarter()
+        Label1.Text = cs.total
+    End Sub
+
+    Private Sub doButton_Click(sender As Object, e As EventArgs) Handles doButton.Click
+        cs.InsertDollar()
+        Label1.Text = cs.total
+    End Sub
+End Class
