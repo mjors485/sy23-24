8384c03712dac6162a8606c3464816c7c7cdbc47
Michael <mjors485@learner.dvusd.org>
2024-02-27 13:01:02 -0700

    2d platformer tutorial

---
+++ 2DPlatTutorial/2DPlatTutorial/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.tmrRight = New System.Windows.Forms.Timer(Me.components)
+        Me.picAir = New System.Windows.Forms.PictureBox()
+        Me.picGround = New System.Windows.Forms.PictureBox()
+        Me.picPlayer = New System.Windows.Forms.PictureBox()
+        Me.tmrLeft = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrUp = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrGameLogic = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrGravity = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        CType(Me.picAir, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picGround, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picPlayer, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tmrRight
+        '
+        Me.tmrRight.Interval = 20
+        '
+        'picAir
+        '
+        Me.picAir.Location = New System.Drawing.Point(-3, -61)
+        Me.picAir.Name = "picAir"
+        Me.picAir.Size = New System.Drawing.Size(805, 525)
+        Me.picAir.TabIndex = 0
+        Me.picAir.TabStop = False
+        '
+        'picGround
+        '
+        Me.picGround.BackColor = System.Drawing.Color.ForestGreen
+        Me.picGround.Location = New System.Drawing.Point(-3, 522)
+        Me.picGround.Name = "picGround"
+        Me.picGround.Size = New System.Drawing.Size(805, 42)
+        Me.picGround.TabIndex = 1
+        Me.picGround.TabStop = False
+        '
+        'picPlayer
+        '
+        Me.picPlayer.BackColor = System.Drawing.Color.White
+        Me.picPlayer.Image = CType(resources.GetObject("picPlayer.Image"), System.Drawing.Image)
+        Me.picPlayer.Location = New System.Drawing.Point(12, 470)
+        Me.picPlayer.Name = "picPlayer"
+        Me.picPlayer.Size = New System.Drawing.Size(85, 68)
+        Me.picPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picPlayer.TabIndex = 2
+        Me.picPlayer.TabStop = False
+        '
+        'tmrLeft
+        '
+        Me.tmrLeft.Interval = 20
+        '
+        'tmrUp
+        '
+        Me.tmrUp.Interval = 20
+        '
+        'tmrGameLogic
+        '
+        Me.tmrGameLogic.Interval = 20
+        '
+        'tmrGravity
+        '
+        Me.tmrGravity.Interval = 20
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.PictureBox1.Location = New System.Drawing.Point(554, 407)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(171, 22)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Tag = "bound"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.PictureBox2.Location = New System.Drawing.Point(76, 364)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(171, 22)
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Tag = "bound"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.PictureBox3.Location = New System.Drawing.Point(365, 175)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(171, 22)
+        Me.PictureBox3.TabIndex = 5
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.Tag = "bound"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SkyBlue
+        Me.ClientSize = New System.Drawing.Size(800, 562)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.picPlayer)
+        Me.Controls.Add(Me.picGround)
+        Me.Controls.Add(Me.picAir)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picAir, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picGround, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picPlayer, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents tmrRight As Timer
+    Friend WithEvents picAir As PictureBox
+    Friend WithEvents picGround As PictureBox
+    Friend WithEvents picPlayer As PictureBox
+    Friend WithEvents tmrLeft As Timer
+    Friend WithEvents tmrUp As Timer
+    Friend WithEvents tmrGameLogic As Timer
+    Friend WithEvents tmrGravity As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+End Class
