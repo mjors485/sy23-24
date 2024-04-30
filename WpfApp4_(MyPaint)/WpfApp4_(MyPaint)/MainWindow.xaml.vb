047dca15a07e93b7624b75abfc157e3d7857afa2
Michael <mjors485@learner.dvusd.org>
2024-04-30 13:05:11 -0700

    wpf paint start

---
+++ WpfApp4_(MyPaint)/WpfApp4_(MyPaint)/MainWindow.xaml.vb
@@ -0,0 +1,18 @@
+﻿Class MainWindow
+    Private Sub drawingCanvas_MouseMove(sender As Object, e As MouseEventArgs) Handles drawingCanvas.MouseMove
+        Dim el As New Ellipse
+        el.Width = widthSlider.Value
+        el.Height = heightSlider.Value
+        el.Fill = colorRectangle1.Fill
+        Dim p As Point = Mouse.GetPosition(drawingCanvas)
+        Canvas.SetLeft(el, p.X)
+        Canvas.SetTop(el, p.Y)
+        If e.LeftButton = MouseButtonState.Pressed Then
+            drawingCanvas.Children.Add(el)
+        End If
+    End Sub
+
+    Private Sub color2Rectangle_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles color2Rectangle.MouseDown, color1Rectangle.MouseDown, color3Rectangle.MouseDown, color4Rectangle.MouseDown
+        colorRectangle1.Fill = sender.fill
+    End Sub
+End Class
