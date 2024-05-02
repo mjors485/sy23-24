9f0bb7837e73f505846d5c4129381a407a5fa90b
Michael <mjors485@learner.dvusd.org>
2024-05-02 12:37:03 -0700

    5/2/24 update

--- WpfApp4_(MyPaint)/WpfApp4_(MyPaint)/MainWindow.xaml.vb
+++ WpfApp4_(MyPaint)/WpfApp4_(MyPaint)/MainWindow.xaml.vb
@@ -1,4 +1,7 @@
-﻿Class MainWindow
+﻿Imports Microsoft.Win32
+Imports System.IO
+
+Class MainWindow
     Private Sub drawingCanvas_MouseMove(sender As Object, e As MouseEventArgs) Handles drawingCanvas.MouseMove
         If shapeLabel.Content = "Elipse" Then
             Dim el As New Ellipse
@@ -40,5 +43,53 @@
 
     Private Sub elipseButton_Click(sender As Object, e As RoutedEventArgs) Handles elipseButton.Click, rectangleButton.Click
         shapeLabel.Content = sender.content
+    End Sub
+
+    Private Sub saveButton_Click(sender As Object, e As RoutedEventArgs) Handles saveButton.Click
+        ExportToPng(drawingCanvas)
+    End Sub
+
+    Public Sub ExportToPng(ByVal surface As Canvas)
+
+        Dim sfd As New SaveFileDialog
+
+        sfd.ShowDialog()
+
+        If sfd.FileName Is Nothing Then Return
+
+        Dim transform As Transform = surface.LayoutTransform
+
+        surface.LayoutTransform = Nothing
+
+        Dim size As Size = New Size(surface.Width, surface.Height)
+
+        surface.Measure(size)
+
+        surface.Arrange(New Rect(size))
+
+        Dim renderBitmap As RenderTargetBitmap = New RenderTargetBitmap(CInt(size.Width), CInt(size.Height), 96.0R, 96.0R, PixelFormats.Pbgra32)
+
+        renderBitmap.Render(surface)
+
+
+
+        Using outStream As FileStream = New FileStream(sfd.FileName & ".png", FileMode.Create)
+
+            Dim encoder As PngBitmapEncoder = New PngBitmapEncoder()
+
+            encoder.Frames.Add(BitmapFrame.Create(renderBitmap))
+
+            encoder.Save(outStream)
+
+        End Using
+
+        '
+
+        surface.LayoutTransform = transform
+
+        Canvas.SetLeft(drawingCanvas, 200)
+
+        Canvas.SetTop(drawingCanvas, 10)
+
     End Sub
 End Class
